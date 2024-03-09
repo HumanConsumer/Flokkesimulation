4c7d9057e01d5748066f16a3f30d04d9e664c540
HumanConsumer <113416857+HumanConsumer@users.noreply.github.com>
2024-03-09 12:44:01 +0100

    update

--- Le_Fishe/Le_fishe.py
+++ Le_Fishe/Le_fishe.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.locals import *
 import random as r
 from youJustGotVectored import Vector as MyVector #importerer vektorer
-from fish import Clownfish, Shark #importerer fisken og hajen
+from fish import Clownfish, Shark, food #importerer fisken, hajen og maden
 import random
 
 
@@ -10,7 +10,7 @@ import random
 clock = pygame.time.Clock() #sætter clock speed
 # Bredde og højde på vinduet
 windowsize=600
-width = windowsize
+width = 1200
 height = windowsize
  
 # Initialisering af Pygame
@@ -23,25 +23,37 @@ font = pygame.font.SysFont("Arial", 36)
 
 def setup():
     pass
-    global cfish1, shark1 #listen over fisk og hajen gøres global
+    global cfish1, shark1, foodman #listen over fisk og hajen gøres global
     cfish1=[] #først skabes en tom liste liste 
+    foodman=[] #dette er listen der skal indeholde mad
     for i in range(50): #der køres nu en for løkke der køres 50 gange
-        cfish1.append(Clownfish(location=MyVector([random.randint(100,300),random.randint(100,300)]),velocity=MyVector([random.randint(-2,2),random.randint(-2,2)]),color=(255,0,0))) #hver gang forløkken køres, bliver der tilføjet en klovnefisk med en tilfældig position og hastighed til listen
-    shark1=Shark(location=MyVector([500,500]),velocity=MyVector([1,1]),color=(200,200,200),target = cfish1[0]) #derefter bliver der skabt en haj
- 
+        cfish1.append(Clownfish(location=MyVector([random.randint(100,300),random.randint(100,300)]),velocity=MyVector([1, 1]),color=(255,0,0))) #hver gang forløkken køres, bliver der tilføjet en klovnefisk med en tilfældig position og hastighed til listen
+    shark1=Shark(location=MyVector([500,500]),velocity=MyVector([1.5,1.5]),color=(200,200,200),target = cfish1[0]) #derefter bliver der skabt en haj
+    for i in range(5): #der køres nu en liste 5 gange som skaber 5 styker mad i en liste.
+        foodman.append(food(location=[random.randint(0,1200),random.randint(0,600)], color=(0,200,0))) #maden bliver skabt med en tilfældig lokation
+        
+    
+    
 def draw():
     clock.tick(30) #clock tick sættes til 30
 
     screen.fill((0, 0, 255)) #sætter baggrunden til blå
 
     #NB! bruger her dependency injection for at undgå circular import dependencies in other files.
-    for i in range(len(cfish1)):
-        cfish1[i].swim(shark1) #for hver fisk i listen cfish1 bliver der kaldt deres swim og show metode
+    for i in range(len(cfish1)): #denne for løkke itererer mellem alle klovnefisk i listen
+        cfish1[i].swim(shark1,foodman) #for hver fisk i listen cfish1 bliver der kaldt deres swim og show metode
         cfish1[i].show(screen)
+        cfish1[i].eat(foodman)
     shark1.consume(cfish1) #hajen kører dens consume, swim og show metode
     shark1.swim()
     shark1.show(screen)
 
+    for i in range(len(foodman)): #der itereres nu over alt maden for at tegne dem
+        foodman[i].show(screen)
+
+    if(len(foodman)<5): #hvis der er mindre end 5 stykker mad i madlisten, bliver der skabt en ny
+        foodman.append(food(location=[random.randint(0,1200),random.randint(0,600)], color=(0,200,0))) #dette stykke mad har en tilfældig lokation når den er skabt
+        
     
     # Vis vindhastighed og retning i vinduet
     text = f"Something"
@@ -49,7 +61,7 @@ def draw():
     screen.blit(text_surface, (10, 50))
  
     pygame.display.update()
- 
+
 setup() #setup kaldes
  
 running = True
--- Le_Fishe/fish.py
+++ Le_Fishe/fish.py
@@ -17,15 +17,45 @@ class Clownfish(fishMoment):
     def __init__(self, location, velocity, color):
         super().__init__(location, velocity, color) #tager atributterne fra superclassen
                 
-    def swim(self,shark):
-        direction=Vector([self.loc[i]-shark.loc[i] for i in range(len(self.loc))]) #Finder afstanden mellem hajen og fisken med en comprehension over alle koordinaterne og laver et array af koordinaterne, denne vektor peger fra hajen mod fisken
-        normalizedDirection=direction.normalize().get_coordinates() #Finder normalvektoren af retningen fra hajen til fisken
+    def swim(self,shark,foody):
+        sharkdistande=(((self.loc[0]-shark.loc[0])**2 + (self.loc[1]-shark.loc[1])**2)**0.5)-shark.size-self.size # finder afstanden til hajen
+        shortest=2000 #en startagstand der bruges til maden
+        fooder=None #denne variabel er den tætteste mad
+        for food in foody: #food er mad objecter og foody er arrayet der indeholder maden
+            fooddistance=(((self.loc[0]-food.loc[0])**2 + (self.loc[1]-food.loc[1])**2)**0.5) #denne finder afstanden til det specifikke mad
+            if(fooddistance< shortest): #Hvis dette stykke mad er tættere på end de andre der kigges på før, bliver dette statement true
+                shortest=fooddistance #dette mad bliver nu sat til at være det tætteste mad
+                fooder=food #den tætteste mad bliver nu sat ind i variablen fooder
+        
+        if sharkdistande< shortest*1.5: #denne ser om maden eller hajen er tættest på fisken. da hajen er hurtigere, skal maden være mindst 50% tættere end hajen før den targeter. Hvis hajen er tættere på end maden, flygter fisken
+            direction=Vector([self.loc[i]-shark.loc[i] for i in range(len(self.loc))]) #Finder retningen mellem hajen og fisken med en comprehension over alle koordinaterne og laver et array af koordinaterne, denne vektor peger fra hajen mod fisken
+            normalizedDirection=direction.normalize().get_coordinates() #Finder normalvektoren af retningen fra hajen til fisken
+        
+        else: #hvis hajen er langt væk, går fisken efter det nærmeste mad fooder
+            direction=Vector([fooder.loc[i]-self.loc[i] for i in range(len(self.loc))]) #Finder retningen mellem maden og fisken med en comprehension over alle koordinaterne og laver et array af koordinaterne, denne vektor peger fra hajen mod fisken
+            normalizedDirection=direction.normalize().get_coordinates() #Finder normalvektoren af retningen fra fisken til maden
+  
+        iteration=2 #da højden er halvdelen af bredden, laves denne variabel. den første iteration er bredden 1200 for x koordinatet, og i anden iteration er højden 600 for y koordinaten. med denne variabel kan man bruge den samme kode, hvor tallene er halvt så store i anden iteration
         for i in range(len(self.loc)):
-            self.loc[i] += normalizedDirection[i] #Her bevæger klovnefisken sig væk fra hajen, da vektoren peger fra hajen til fisken, svømmer den direkte væk fra hajen
-            if self.loc[i] > 600: #hvis fisken rammer kanten så teleporterer den ind i midten, dette er lavet så fisken ikke sidder fast i kanten
-                self.loc[i]=200
+            self.loc[i] += normalizedDirection[i]*self.vel[i] #Her bevæger klovnefisken sig enten væk fra hajen elelr mod maden, hvis maden er tættere end hajen
+            if self.loc[i] > 600*iteration: #hvis fisken rammer kanten så teleporterer den ind i midten, dette er lavet så fisken ikke sidder fast i kanten
+                self.loc[i]=100*iteration
             elif self.loc[i] < 0:
-                self.loc[i]=400
+                self.loc[i]=500*iteration
+            iteration-=1
+                
+    def eat(self, food_list):
+        to_remove=[] #Da der er en chance for at fisken spiser flere forskellige stykker mad i en frame, laves denne liste der skal indeholde alt det mad som fisken rammer
+        for food in food_list: #der itereres gennem alle mad i listen af mad
+            distance = ((self.loc[0] - food.x) ** 2 + (self.loc[1] - food.y) ** 2) ** 0.5 #Først findes afstanden mellem klovnefisken og maden
+            if distance < (self.size + food.size): #Hvis maden rammer en klovnefisk, bliver klovnefisken større og maden bliver slettet fra listen
+                self.size = self.size*2 #klovnefisken bliver dobbelt så stor for hver mad den rammer
+                to_remove.append(food) #den mad som fisken rammer kommer ind i to remove listen
+                self.vel[0]*0.9 #da den bliver større, bliver den 10% langsommere
+                self.vel[1]*0.9
+        
+        for food in to_remove: #her kører en for løkke over to remove listen og fjerner alt det mad fisken har rørt
+            food_list.remove(food)
                         
 class Shark(fishMoment):
     def __init__(self, location, velocity, color,target):
@@ -37,22 +67,35 @@ class Shark(fishMoment):
         normalizedDirection=direction.normalize().get_coordinates() #Der findes normalvektoren for retningen, og der tages derefter dens koordinater som bliver til variablne nomalizeddirection
         for i in range(len(self.loc)):
             self.loc[i] += self.vel[i]*normalizedDirection[i] #her bevæger hajen sig mod fisken med normalvektoren som retning
-            if self.loc[i] > 600: #hvis hajen rammer den ene side af skærmen teleporterer den over i den anden side
-                self.loc[i]=0
+            if self.loc[i] > 1200: #hvis hajen rammer den ene side af skærmen teleporterer den over i den anden side
+                self.loc[i]=0 #da hajens eneste formål er at følge efter fisken, bevæger den sig aldrig ud af skærmen da fiskene heller ikke kan gøre dette. Denne kode er der som en nødsituation hvis den alligevel ender med at bevæge sid ud af skærmen. 
             elif self.loc[i] < 0:
-                self.loc[i]=600
+                self.loc[i]=1200
 
          
     def consume(self, clownfish_list):
-        newtarget=1000 #dette er en startværdi som bruges til at finde fiskens afstand til hajen, denne værdi kommer kun til at falde gennem funktionen
+        newtarget=2000 #dette er en startværdi som bruges til at finde fiskens afstand til hajen, denne værdi kommer kun til at falde gennem funktionen
         target2=0 #Dette er den specifikke klovnefisk som hajen skal jage
         for clownfish in clownfish_list: #der itereres gennem alle klovnefisk i listen af klovnefisk
             distance = ((self.loc[0] - clownfish.loc[0]) ** 2 + (self.loc[1] - clownfish.loc[1]) ** 2) ** 0.5 #Først findes afstanden mellem klovnefisken og hajen
             if distance < (self.size + clownfish.size): #Hvis hajen rammer en klovnefisk, bliver hajen større og klovnefisken bliver slettet fra listen
-                self.size += 1
-                clownfish_list.remove(clownfish)
+                self.size += 1 #hajen bliver 1 større når den rammer klovnefisken
+                clownfish_list.remove(clownfish) #klovnefisken bliver fjernet fra listen
+                target2=clownfish_list[0] #der bliver med det samme sat et nyt target, så denne variabel ikke bliver invalid
             elif distance < newtarget: #Hvis ikke der rammes en fisk, finder hajen en klovnefisk at gå efter. Hvis afstanden er mindre end newtarget, er der fundet en klovnefisk der er tættere på og if statementet aktiveres
                 newtarget = distance #nu bliver newtarget sat til distance så der nu er en ny afstand som skal overgås
                 target2=clownfish #target2 bliver sat til at være den specifikke klovnefisk som er tættest på lige nu
         self.target = target2 #Når for løkken er færdig, bliver self.target sat til at være den specifikke klovnefisk
-        return len(clownfish_list) < 1
+        return len(clownfish_list) < 1 #denne kode sender enten en true eller false for at se om alle fiskene er spist
+    
+    
+class food(): #dette objekt er maden der skal spises, den har kun en show metode da resten af dens funktioner ligger ovre i klovnefisken
+    def __init__(self, location, color):
+        self.loc=location
+        self.x=location[0]
+        self.y=location[1]
+        self.color=color
+        self.size=8
+    
+    def show(self, surface):
+        pygame.draw.circle(surface, self.color, self.loc, self.size)
